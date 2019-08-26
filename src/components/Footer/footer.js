739f28ae06708aa0c75ac3a3aae2a0c1f005f64c
Santiago Bedoya B <santiagob@experimentality.co>
2019-08-26 02:27:14 -0500

    Added footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import './footer.scss';
+
 class Footer extends Component {
 
     state = { users: [] };
 
     render() {
         return (
-            <div className="">Footer Works</div>
+            <div className="ui secondary pointing footer">Footer Works</div>
         );
     }
 }
