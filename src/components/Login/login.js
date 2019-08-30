2cc0ea573467e7246c4dde17f13cead02da6b322
Santiago Bedoya B <santiagob@experimentality.co>
2019-08-29 21:11:48 -0500

    Added login with google through firebase

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,42 +1,73 @@
 import React, { Component } from 'react';
 
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../../firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+    googleProvider: new firebase.auth.GoogleAuthProvider().setCustomParameters({ hd: 'udea.edu.co' }),
+};
+
 class Login extends Component {
 
     state = { users: [], appName: 'AulApp' };
 
     render() {
+        const {
+            user,
+            signOut,
+            signInWithGoogle,
+        } = this.props;
+
         return (
-            <div class="ui middle aligned center aligned grid">
-                <div class="column">
-                    <h2 class="ui teal image header">
-                        <div class="content">
+            <div className="ui middle aligned center aligned grid">
+                <div className="column">
+                    <h2 className="ui teal image header">
+                        <div className="content">
                             Administración - {this.state.appName}
                         </div>
                     </h2>
-                    <form class="ui large form">
-                        <div class="ui stacked segment">
-                            <div class="field">
-                                <div class="ui left icon input">
-                                    <i class="user icon"></i>
+                    <form className="ui large form">
+                        <div className="ui stacked segment">
+                            <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="user icon"></i>
                                     <input type="text" name="email" placeholder="Usuario" />
                                 </div>
                             </div>
-                            <div class="field">
-                                <div class="ui left icon input">
-                                    <i class="lock icon"></i>
+                            <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="lock icon"></i>
                                     <input type="password" name="password" placeholder="Contraseña" />
                                 </div>
                             </div>
-                            <div class="ui fluid large teal submit button">
+                            <div className="ui fluid large teal submit button">
                                 Login
                             </div>
                         </div>
-                        <div class="ui error message"></div>
+                        <div className="ui error message"></div>
                     </form>
+                    {
+                        user
+                            ? <p>Hello, {user.displayName}</p>
+                            : <p>Please sign in.</p>
+                    }
+                    {
+                        user
+                            ? <button onClick={signOut}>Sign out</button>
+                            : <button onClick={signInWithGoogle}>Sign in with Google</button>
+                    }
                 </div>
             </div>
         );
     }
 }
 
-export default Login;
+export default withFirebaseAuth({
+    providers,
+    firebaseAppAuth,
+})(Login);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD52qeK3mG0YxXaewcDs8dE3kxQI6lS8rk",
+    authDomain: "integrador-jsd.firebaseapp.com",
+    databaseURL: "https://integrador-jsd.firebaseio.com",
+    projectId: "integrador-jsd",
+    storageBucket: "",
+    messagingSenderId: "553884378564",
+    appId: "1:553884378564:web:db7bac27e57c0afe"
+};
+
+export default firebaseConfig;
