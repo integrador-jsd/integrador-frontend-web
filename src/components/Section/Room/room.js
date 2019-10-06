0ad6f718e0dfdd214e13eda1489e6ff7a999eaa8
Santiago Bedoya B <santiagob@experimentality.co>
2019-10-06 03:27:14 -0500

    Added Acordions and list to spaces (Add or remove)

--- src/components/Section/Room/room.js
+++ src/components/Section/Room/room.js
@@ -1,37 +1,91 @@
-import React, { Component } from 'react'
-import { Accordion, Grid, Header } from 'semantic-ui-react'
-import './room.scss'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Accordion, Grid, Header, Table, Select, Button, Icon } from 'semantic-ui-react';
+import './room.scss';
+
+import { typeRoom, typeSectional } from '../../../util/methods';
+
+import { getSections } from '../../../actions/sectionActions';
 
 class Room extends Component {
 
-    level1Panels = () => [
-        { key: 'panel-1a', title: 'Level 1A', content: 'Level 1A Contents' },
-        { key: 'panel-ba', title: 'Level 1B', content: 'Level 1B Contents' },
-    ];
+    componentDidMount() {
+        const logisticUnit = this.props.email.split('@')[0];
+        this.props.getSections(this.props.idToken, logisticUnit);
+    }
 
-    level1Content = () => (
-        <div>
-            Welcome to level 1
-          <Accordion.Accordion panels={this.level1Panels()} />
-        </div>
+    TableData = (roomsPerSection, type) => (
+        <Table unstackable>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>Seccional</Table.HeaderCell>
+                    <Table.HeaderCell>Id</Table.HeaderCell>
+                    <Table.HeaderCell>Tipo</Table.HeaderCell>
+                    <Table.HeaderCell>Capacidad</Table.HeaderCell>
+                    <Table.HeaderCell>Acción</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+                {
+                    roomsPerSection.map(({ id, sectionalID, blockID, roomID }) => {
+                        return (
+                            <Table.Row key={id}>
+                                <Table.Cell>{typeSectional(sectionalID)}</Table.Cell>
+                                <Table.Cell>{`${blockID}-${roomID}`}</Table.Cell>
+                                <Table.Cell>{typeRoom(2)}</Table.Cell>
+                                <Table.Cell>40</Table.Cell>
+                                <Table.Cell>
+                                    {
+                                        !type
+                                            ? <Select placeholder='Sin Asignar' options={
+                                                [
+                                                    { key: 'af', value: 'af', text: 'Afghanistan' },
+                                                    { key: 'ax', value: 'ax', text: 'Aland Islands' },
+                                                    { key: 'al', value: 'al', text: 'Albania' }
+                                                ]
+                                            } />
+                                            : <Button color='red' content='Eliminar' />
+                                    }
+                                </Table.Cell>
+                            </Table.Row>
+                        )
+                    })
+                }
+            </Table.Body>
+
+            <Table.Footer fullWidth style={{ display: !type ? '' : 'none' }}>
+                <Table.Row>
+                    <Table.HeaderCell colSpan='5'>
+                        <Button
+                            floated='right'
+                            icon
+                            labelPosition='left'
+                            primary
+                            size='small'
+                        >
+                            <Icon name='save' />
+                            Guardar Cambios
+                         </Button>
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Footer>
+        </Table>
     );
 
-    level2Panels = () => [
-        { key: 'panel-2a', title: 'Level 2A', content: 'Level 2A Contents' },
-        { key: 'panel-2b', title: 'Level 2B', content: 'Level 2B Contents' },
-    ];
-
-    level2Content = () => (
-        <div>
-            Welcome to level 2
-          <Accordion.Accordion panels={this.level2Panels()} />
-        </div>
-    );
-
-    rootPanels = () => [
-        { key: 'panel-1', title: 'Level 1', content: { content: this.level1Content() } },
-        { key: 'panel-2', title: 'Level 2', content: { content: this.level2Content() } },
-    ];
+    rootPanels = () => {
+        return this.props.sections.map(({ id, name, rooms_per_sections }) => {
+            return {
+                key: id,
+                title: name,
+                content: {
+                    content: rooms_per_sections.length > 0
+                        ? this.TableData(rooms_per_sections, true)
+                        : <div>Acá no hay nadita</div>
+                }
+            }
+        });
+    }
 
     render() {
         return (
@@ -40,7 +94,9 @@ class Room extends Component {
                     <Header as='h2' color='green'>Espacios</Header>
                 </Grid.Row>
                 <Grid.Row className="row">
-                    <Accordion defaultActiveIndex={[0, 2]} panels={this.rootPanels()}
+                    <Accordion defaultActiveIndex={0} panels={this.rootPanels()}
+                        exclusive={true} styled />
+                    <Accordion defaultActiveIndex={[0]} panels={this.rootPanels()}
                         exclusive={false} styled />
                 </Grid.Row>
             </Grid>
@@ -48,4 +104,14 @@ class Room extends Component {
     }
 }
 
-export default Room;
+const mapStateToProps = (state) => {
+    console.log(state);
+
+    return {
+        idToken: state['user']['idToken'],
+        email: state['user']['email'],
+        sections: state['section']['list']
+    }
+}
+
+export default connect(mapStateToProps, { getSections })(Room);
