8d3b74240dfe8e763731aef3992e9a63ed6eaf37
Santiago Bedoya B <santiagob@experimentality.co>
2019-10-13 16:42:48 -0500

    Added modal to show details about selected pending request

--- src/components/Request/ShowRequest/showRequest.js
+++ src/components/Request/ShowRequest/showRequest.js
@@ -1,27 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Table, Icon } from 'semantic-ui-react';
+import { Table, Icon, Button, Grid } from 'semantic-ui-react';
 
 import { getPendingRequests } from '../../../actions/requestActions';
 import { typeSectional } from '../../../util/methods';
+import PrivateModal from '../../PrivateModal/privateModal';
+import { openModal, closeModal } from '../../../actions/modalActions';
 
 class ShowRequest extends Component {
 
-    state = { logisticUnit: '' };
+    state = { logisticUnit: '', selectedRequest: {} };
 
     componentDidMount() {
         const logisticUnit = this.props.email.split('@')[0];
         this.props.getPendingRequests(this.props.idToken, logisticUnit);
     }
 
+    openRequestDetails = (request) => {
+        this.setState({ selectedRequest: request });
+        this.props.openModal({ size: 'small', open: true });
+    }
+
+    getRequestContent = () => {
+        const request = this.state.selectedRequest;
+        return (
+            <Table>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell><b> Solicitante: </b></Table.Cell>
+                        <Table.Cell>{request.createdBy}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell><b> Seccional: </b></Table.Cell>
+                        <Table.Cell>{request.sectionalID}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell><b> Hora inicial: </b></Table.Cell>
+                        <Table.Cell>{request.startTime}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell><b> Hora final: </b></Table.Cell>
+                        <Table.Cell>{request.endTime}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell><b> Aula: </b></Table.Cell>
+                        <Table.Cell>{`${request.blockID}-${request.roomID}`}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell><b> Descripción: </b></Table.Cell>
+                        <Table.Cell>{request.description}</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+        );
+    }
+
+    getRequestActions = () => {
+        return (
+            <Grid centered columns={2} >
+                <Grid.Row>
+                    <Grid.Column>
+                        <Button color='grey' onClick={() => this.props.closeModal()}>
+                            <Icon name='arrow left' /> Atras
+                    </Button>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Button color='red' onClick={() => this.props.closeModal()}>
+                            <Icon name='delete' />  Rechazar
+                        </Button>
+                        <Button color='green' onClick={() => this.props.closeModal()}>
+                            <Icon name='checkmark' />  Confirmar
+                        </Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+
+        );
+    }
+
     listPendingRequests = () => {
-        return this.props.pendingRequests.map(({ id, sectionalID, blockID, roomID, createdBy }) => {
+        return this.props.pendingRequests.map((request) => {
             return (
-                <Table.Row key={id}>
-                    <Table.Cell>{typeSectional(sectionalID)}</Table.Cell>
-                    <Table.Cell>{`${blockID}-${roomID}`}</Table.Cell>
-                    <Table.Cell>{createdBy}</Table.Cell>
-                    <Table.Cell><Icon name='eye' style={{ cursor: 'pointer' }} /></Table.Cell>
+                <Table.Row key={request.id}>
+                    <Table.Cell>{typeSectional(request.sectionalID)}</Table.Cell>
+                    <Table.Cell>{`${request.blockID}-${request.roomID}`}</Table.Cell>
+                    <Table.Cell>{request.createdBy}</Table.Cell>
+                    <Table.Cell><Icon name='eye' style={{ cursor: 'pointer' }} onClick={() => this.openRequestDetails(request)} /></Table.Cell>
                 </Table.Row>
             );
         });
@@ -29,25 +93,32 @@ class ShowRequest extends Component {
 
     render() {
         return (
-            <Table unstackable>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Seccional</Table.HeaderCell>
-                        <Table.HeaderCell>Aula</Table.HeaderCell>
-                        <Table.HeaderCell>Solicitante</Table.HeaderCell>
-                        <Table.HeaderCell>Detalle</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
+            <Fragment>
+                <PrivateModal content={this.getRequestContent()} header="Detalles de la solicitud"
+                    actions={this.getRequestActions()} />
+                <Table unstackable>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Seccional</Table.HeaderCell>
+                            <Table.HeaderCell>Aula</Table.HeaderCell>
+                            <Table.HeaderCell>Solicitante</Table.HeaderCell>
+                            <Table.HeaderCell>Detalle</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
 
-                <Table.Body>
-                    {this.listPendingRequests()}
-                </Table.Body>
-            </Table>
+                    <Table.Body>
+                        {this.listPendingRequests()}
+                    </Table.Body>
+                </Table>
+            </Fragment>
+
         );
     }
 }
 
 const mapStateToProps = (state) => {
+    console.log(state);
+
     return {
         idToken: state['user']['idToken'],
         email: state['user']['email'],
@@ -55,4 +126,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, { getPendingRequests })(ShowRequest);
+export default connect(mapStateToProps, { getPendingRequests, openModal, closeModal })(ShowRequest);
